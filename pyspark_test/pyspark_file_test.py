27a3643aca4ffdba6a943baf247aa3445273c9fc
test python <faisalattar72@gmail.com>
2021-05-30 22:11:29 +0530

    fourth

---
+++ pyspark_test/pyspark_file_test.py
@@ -0,0 +1,5 @@
+from pyspark.sql import  SparkSession
+spark = SparkSession.builder.appName("intelli_pyspark").master("local").getOrCreate()
+print("this is pyspark proj.")
+df=spark.range(100)
+print(df.sample(0.06).collect())
